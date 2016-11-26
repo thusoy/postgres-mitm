bd6a37983763d8793f9f59d9e5e99266820af6cd
Tarjei Husøy <git@thusoy.com>
2016-11-26 15:25:50 -0800

    Set SO_REUSEPORT on the listening socket

--- postgres_mitm.py
+++ postgres_mitm.py
@@ -87,6 +87,7 @@ def main():
     target_backend = args.backend
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     sock.bind(('0.0.0.0', args.port))
 
     # max queued connections
